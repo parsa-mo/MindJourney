c79a2b4380af87ad69c6035414a63845176b8fc3
Parsa <mo.parsa@yahoo.com>
2024-10-02 13:26:48 +1000

    Remove Google Implementation

--- app/src/main/java/com/example/mindjourney/MainActivity.kt
+++ app/src/main/java/com/example/mindjourney/MainActivity.kt
@@ -19,78 +19,25 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.*
-import com.example.mindjourney.login.GoogleAuthClient
-import com.example.mindjourney.login.SignInScreen
-import com.example.mindjourney.login.SignInViewModel
 import com.example.mindjourney.screens.* // Import your existing screens
 import com.example.mindjourney.ui.theme.MindJourneyTheme
-import com.google.android.gms.auth.api.identity.Identity
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
-    val googleAuthClient by lazy {
-        GoogleAuthClient(
-            context = applicationContext,
-            oneTapClient = Identity.getSignInClient(applicationContext)
-        )
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                MainApp(googleAuthClient)
+                MainApp()
             }
         }
 }
 
 @Composable
-fun MainApp(googleAuthClient: GoogleAuthClient) {
+fun MainApp() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "sign_in") {
-        composable("sign_in") {
-            val viewModel = viewModel<SignInViewModel>()
-            val state by viewModel.state.collectAsStateWithLifecycle()
-            val lifecycleOwner = LocalLifecycleOwner.current
-            val launcher = rememberLauncherForActivityResult(
-                contract = ActivityResultContracts.StartIntentSenderForResult(),
-                onResult = { result ->
-                    if (result.resultCode == RESULT_OK){
-                        lifecycleOwner.lifecycleScope.launch {
-                            val signInResult = googleAuthClient.signInWithIntent(
-                                intent = result.data ?: return@launch
-                            )
-                            viewModel.onSignInResult(signInResult)
-                        }
-                    }
-                }
-            )
-
-            val context = LocalContext.current
-            val applicationContext = context.applicationContext
-            LaunchedEffect(key1 = state.isSignInSuccessful) {
-                if (state.isSignInSuccessful) {
-                    Toast.makeText(
-                        applicationContext,
-                        "Sign in Successful!",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            }
-            
-            SignInScreen(
-                state = state,
-                onSignInClick = {
-                    lifecycleOwner.lifecycleScope.launch {
-                        val signInIntentSender = googleAuthClient.signIn()
-                        launcher.launch(
-                            IntentSenderRequest.Builder(
-                                signInIntentSender ?: return@launch
-                            ).build()
-                        )
-                    }
-                }
-            )
-        }
+    NavHost(navController = navController, startDestination = "home") {
         composable("home") { HomepageScreen(navController) }  // Homepage screen
         composable("login") { LoginScreen(navController) }    // Login screen
         composable("signup") { SignupScreen(navController) }  // Signup screen
@@ -103,6 +50,6 @@ fun MainApp(googleAuthClient: GoogleAuthClient) {
 @Composable
 fun DefaultPreview() {
     MindJourneyTheme {
-
+        MainApp()
     }
 }
--- app/src/main/java/com/example/mindjourney/login/SignInState.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mindjourney.login
-
-data class SignInState(
-  val isSignInSuccessful: Boolean = false,
-    val signInError: String? = null
-)
